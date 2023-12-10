7b0aca0d520481f73dc006193cff24821c69ce4b
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-10 21:59:06 +0200

    Add non operational error middleware

--- snapshort-backend/src/snapshort-backend/errorHandlers.js
+++ snapshort-backend/src/snapshort-backend/errorHandlers.js
@@ -1,48 +1,104 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+
+  constructor(message, statusCode, isOperational=true) {
     super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+
     this.statusCode = statusCode;
-    this.isOperational = true;
+    this.isOperational = isOperational;
 
     Error.captureStackTrace(this, this.constructor);
   }
+
 }
 
-module.exports.AppError = AppError;
+
+class ErrorHandler {
+
+  constructor(logger) {
+    this.logger = logger;
+  }
+
+  async logError(err) {
+    this.logger.error(err);
+  }
+
+  isTrustedError(err) {
+    return err instanceof AppError && err.isOperational;
+  }
+}
 
 
-module.exports.asyncErrorHandler = (func) => {
+const logger = console;
+const errorHandler = new ErrorHandler(logger);
+
+
+asyncErrorHandler = (func) => {
   return (req, res, next) => {
     func(req, res, next).catch(err => next(new AppError(err.message, 500)));
   };
 }
 
 
-module.exports.errorHandler = (error, req, res, next) => {
+errorHandlerMiddleware = async(error, req, res, next) => {
+  if (!errorHandler.isTrustedError(error))
+    return next(error);
+
+  await errorHandler.logError(error);
   error.statusCode = error.statusCode || 500;
+
   res.jsonResponse(error, error.statusCode);
 };
 
 
-module.exports.uncaughtExceptionHandler = (err) => {
-  console.log('[!] UNCAUGHT EXCEPTION! Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
+nonOperationalErrorHandlerMiddleware = async(error, req, res, next) => {
+  if (errorHandler.isTrustedError(error))
+    return next(error);
+
+  await errorHandler.logError(error);
+  error.statusCode = 500;
+  error.message = 'Something went wrong!';
+
+  res.jsonResponse(error, error.statusCode);
 };
 
 
-module.exports.unhandledRejectionHandler = (err, server) => {
+uncaughtExceptionHandler = async(err, server) => {
+  console.log('[!] UNCAUGHT EXCEPTION! Shutting down...');
+  await errorHandler.logError(err);
+  if (!errorHandler.isTrustedError(err)){
+    server.close(() => {
+      console.log('[!] Server closed...');
+    });
+    process.exit(1);
+  }
+};
+
+
+unhandledRejectionHandler = async(err, server) => {
   console.log('[!] UNHANDLED REJECTION! Shutting down...');
-  console.log(err.name, err.message);
+
+  await errorHandler.logError(err);
   server.close(() => {
     console.log('[!] Server closed...');
   });
 };
 
 
-module.exports.handleSIGTERM = (server) => {
+handleSIGTERM = (server) => {
   console.log('[!] SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
     console.log('[+] Process terminated!');
   });
+};
+
+
+module.exports = {
+  AppError,
+  errorHandlerMiddleware,
+  nonOperationalErrorHandlerMiddleware,
+  uncaughtExceptionHandler,
+  unhandledRejectionHandler,
+  handleSIGTERM,
+  asyncErrorHandler,
 };
