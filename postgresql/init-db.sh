84825f43a130dc87680ba1766d688c64fd0251e5
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-13 19:19:26 +0200

    Alter db user permissions in init file

--- postgresql/init-db.sh
+++ postgresql/init-db.sh
@@ -3,6 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     ALTER ROLE $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';
+    ALTER ROLE $POSTGRES_USER WITH LOGIN;
     ALTER ROLE $POSTGRES_USER SET client_encoding TO 'utf8';
     ALTER ROLE $POSTGRES_USER SET timezone TO 'UTC';
     ALTER ROLE $POSTGRES_USER SET default_transaction_isolation TO 'read committed';
