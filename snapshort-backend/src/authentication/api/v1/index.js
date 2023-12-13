8d128dc67556934d22678fb4cc9809162a5e3b1e
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-13 19:30:33 +0200

    Add authentication app routes

---
+++ snapshort-backend/src/authentication/api/v1/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        res.jsonResponse(null, 200);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+module.exports = router;
