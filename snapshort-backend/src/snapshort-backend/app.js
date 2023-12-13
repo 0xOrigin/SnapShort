0a708aea59e29fa98d7b7a889c6b5f32a5459be4
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-13 21:46:38 +0200

    Load static files in express

--- snapshort-backend/src/snapshort-backend/app.js
+++ snapshort-backend/src/snapshort-backend/app.js
@@ -35,6 +35,7 @@ app.use(
 
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(express.json({ limit: '10kb' }));
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 
 app.use(cors());
