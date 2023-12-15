62c15954a2f360710cb5ca1e422344c4c7d39d5b
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-15 23:21:20 +0200

    Fix constants export

--- snapshort-backend/src/snapshort-backend/constants.js
+++ snapshort-backend/src/snapshort-backend/constants.js
@@ -1,6 +1,10 @@
 const SALT_ROUNDS = 10;
+const SECRET_KEY = process.env.SECRET_KEY || 'secret';
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1d';
 
 
-module.exports.constants = {
-    SALT_ROUNDS
+module.exports = {
+    SALT_ROUNDS,
+    SECRET_KEY,
+    JWT_EXPIRES_IN
 };
