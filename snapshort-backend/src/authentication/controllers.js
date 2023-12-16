64020b46319f5e9d7c00bcfc639d4ccfc84f76f3
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-16 22:32:00 +0200

    Add user, auth controllers

--- snapshort-backend/src/authentication/controllers.js
+++ snapshort-backend/src/authentication/controllers.js
@@ -1,101 +1,28 @@
-const path = require('path');
 const models = require('./models');
-const { asyncErrorHandler, AppError } = require(path.resolve(__dirname, './../snapshort-backend/errorHandlers'));
+const services = require('./services');
+const BaseController = require('./../snapshort-backend/baseController');
+const { asyncErrorHandler } = require('./../snapshort-backend/errorHandlers');
 
 
-const getUsers = asyncErrorHandler(async (req, res, next) => {
-    const users = await models.User.findAll({
-        attributes: {
-            exclude: ['password']
-        }
-    });
-    res.jsonResponse(users, 200);
-});
+class UserController extends BaseController {
+  constructor() {
+    super(models.User, services.UserService);
+  }
+}
 
+class AuthController extends BaseController {
 
-const getUser = asyncErrorHandler(async (req, res, next) => {
-    const user = await models.User.findOne({
-        where: {
-            id: req.params.userId
-        },
-        attributes: {
-            exclude: ['password']
-        }
-    });
-    res.jsonResponse(user, 200);
-});
+  constructor() {
+    super(models.User, services.AuthService);
+  }
 
-
-const createUser = asyncErrorHandler(async (req, res, next) => {
-    const user = await models.User.create({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-    }, {
-        returning: true,
-        attributes: {
-            exclude: ['password']
-        }
-    });
-    res.jsonResponse(user, 201);
-});
-
-
-const updateUser = asyncErrorHandler(async (req, res, next) => {
-    const user = await models.User.update({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-    }, {
-        where: {
-            id: req.params.userId
-        },
-        individualHooks: true,
-        returning: true
-    });
-    if (user[0] === 0) 
-        return next(new AppError('Not found', 404));
-    res.jsonResponse(user[1], 200);
-});
-
-
-const partialUpdateUser = asyncErrorHandler(async (req, res, next) => {
-    let user = await models.User.update({
-        ...req.body
-    }, {
-        where: {
-            id: req.params.userId
-        },
-        individualHooks: true,
-        returning: true
-    });
-    if (user[0] === 0) 
-        return next(new AppError('Not found', 404));
-    res.jsonResponse(user[1], 200);
-});
-
-
-const destroyUser = asyncErrorHandler(async (req, res, next) => {
-    const isDeleted = await models.User.destroy({
-        where: {
-            id: req.params.userId
-        }
-    });
-    if (!isDeleted)
-        return next(new AppError('Not found', 404));
-    res.jsonResponse(null, 204);
-});
-
+  login = asyncErrorHandler(async (req, res, next) => {
+    const token = await this.service.login(req);
+    res.jsonResponse({ token }, 200);
+  });
+}
 
 module.exports = {
-    getUsers,
-    getUser,
-    createUser,
-    updateUser,
-    partialUpdateUser,
-    destroyUser
+  UserController,
+  AuthController,
 };
